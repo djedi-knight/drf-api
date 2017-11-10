a5e63a9c6471f4956461023c13841f460fa822a6
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-09 23:39:58 -0500

    updated settings.py

--- auth_api/auth_api/settings.py
+++ auth_api/auth_api/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# User model
+
+AUTH_USER_MODEL = 'users.User'
 
 # Application definition
 
@@ -37,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'todos',
+    'users',
 ]
 
 MIDDLEWARE = [
