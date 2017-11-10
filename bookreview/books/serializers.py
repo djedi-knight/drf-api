f2065b65ce77c09a2329bae3ca0a09418417a22f
Shawn Daichendt <sdaichendt@alterisgroup.com>
2017-11-10 16:24:16 -0500

    created AuthorSerializer

--- bookreview/books/serializers.py
+++ bookreview/books/serializers.py
@@ -1,2 +1,11 @@
 from rest_framework import serializers
 from .models import Author, Book
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    """
+    Serializing all the Authors
+    """
+    class Meta:
+        model = Author
+        fields = ('id', 'first_name', 'last_name')
